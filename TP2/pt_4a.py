df47d6aef8426538a8ccd202c957ddfa753c9951
Agustin978 <loboagus10@gmail.com>
2022-04-19 09:18:39 -0300

    Agregadas las graficas de las otras funciones pedidas en el punto 4

--- TP2/pt_4a.py
+++ TP2/pt_4a.py
@@ -3,9 +3,15 @@ import matplotlib.pyplot as plt
 
 x = np.linspace(-4, 4, 150)
 
-funcion = x**2 - 2*x -3
+funcion_1 = x**2 - 2*x -3
+funcion_2 = np.sqrt((2*x)+3)
+funcion_3 = ((x**2)-3)/2
+funcion_4 = 3/(x-2)
 
 plt.figure(1,(12,8),55,'w','g')
 plt.subplot(2,1,1)
-plt.plot(x, funcion)
+plt.plot(x, funcion_1)
+plt.plot(x, funcion_2)
+plt.plot(x, funcion_3)
+plt.plot(x, funcion_4)
 plt.show()
